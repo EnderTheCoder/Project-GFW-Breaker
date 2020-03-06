5e90c9dbccc76c16b6f813c80704436be87f1268
ender <1991455223@qq.com>
2020-03-06 15:09:40 +0800

    彻底完成blog页面，添加markdown解析器js

--- js/blog.js
+++ js/blog.js
@@ -1,42 +1,91 @@
-let json = {};
-json.app_id = 1;
-json.type = 'all';
-json.timestamp = getUnixTS();
-json.sign = $.md5('app_id1timestamp' + json.timestamp + 'type' + json.type + '6ab43fb5a4d624f9fa000bc83ccef011');
-json.sign = json.sign.toUpperCase();
-$.ajax({
-    url: 'API/blog.php',
-    type: "POST",
-    dataType: 'json',
-    timeout: 5000,
-    data: json,
-    success: function (result) {
-        let json = eval(result);
-        switch (json['code']) {
-            case 100:
-                for (let i = 0; i < json['data']['row']; i++) {
-                    $("#blog-append-area").append(
-                        '<div class="layui-col-xs12 layui-col-sm4 layui-col-md3 blog-frame">\n' +
-                        '            <div class="blog-block">\n' +
-                        '                <h1>\n' +
-                        json['data'][i]['title'] +
-                        '                </h1>\n' +
-                        '                <p>\n' +
-                        json['data'][i]['summary'] +
-                        '                </p>\n' +
-                        '            </div>\n' +
-                        '        </div>'
-                    );
-                }
-                break;
-            case 213:
-                layer.msg('您太快了!慢点吧');
-                break;
-            default:
-                layer.msg('发生了未知的错误');
-        }
-    },
-    error: function () {
+let id = getQueryString('page');
+if (id) {
+    $(".layui-container").remove();
+    $("body").append("<h1 id='blog-title'></h1>" +
+        "<div id='blog-author'></div>" +
+        "<div id='blog-time'></div>" +
+        "<div id='blog-content'></div>");
+    let json = {};
+    json.app_id = 1;
+    json.type = 'single';
+    json.id = id;
+    json.timestamp = getUnixTS();
+    json.sign = $.md5('app_id1id' + json.id + 'timestamp' + json.timestamp + 'type' + json.type + '6ab43fb5a4d624f9fa000bc83ccef011');
+    json.sign = json.sign.toUpperCase();
+    $.ajax({
+        url: 'API/blog.php',
+        type: "POST",
+        dataType: 'json',
+        timeout: 5000,
+        data: json,
+        success: function (result) {
+            let json = eval(result);
+            switch (json['code']) {
+                case 100:
+                    let converter = new Markdown.Converter();
+                    let html = converter.makeHtml(json['data'][0]['content']);
+                    $("#blog-title").html(json['data'][0]['title']);
+                    $("#blog-author").html('作者:' + json['data'][0]['author']);
+                    $("#blog-time").html('时间:' + getDate(json['data'][0]['timestamp'], 'yyyy-MM-dd hh:mm'));
+                    $("#blog-content").html(html);
+                    break;
+                case 213:
+                    layer.msg('您太快了!慢点吧');
+                    break;
+                default:
+                    layer.msg('发生了未知的错误');
+            }
+        },
+        error: function () {
 
-    }
-});
+        }
+    });
+} else {
+    let json = {};
+    json.app_id = 1;
+    json.type = 'all';
+    json.timestamp = getUnixTS();
+    json.sign = $.md5('app_id1timestamp' + json.timestamp + 'type' + json.type + '6ab43fb5a4d624f9fa000bc83ccef011');
+    json.sign = json.sign.toUpperCase();
+    $.ajax({
+        url: 'API/blog.php',
+        type: "POST",
+        dataType: 'json',
+        timeout: 5000,
+        data: json,
+        success: function (result) {
+            let json = eval(result);
+            switch (json['code']) {
+                case 100:
+                    for (let i = 0; i < json['data']['row']; i++) {
+                        $("#blog-append-area").append(
+                            '<div class="layui-col-xs12 layui-col-sm4 layui-col-md3 blog-frame" id="' +
+                            'row-' + json['data'][i]['id'] +
+                            '">\n' +
+                            '            <div class="blog-block">\n' +
+                            '                <h1>\n' +
+                            json['data'][i]['title'] +
+                            '                </h1>\n' +
+                            '                <p>\n' +
+                            json['data'][i]['summary'] +
+                            '                </p>\n' +
+                            '            </div>\n' +
+                            '        </div>'
+                        );
+                        $("#row-" + json['data'][i]['id']).click(function () {
+                            window.location.href = 'blog.html?page=' + json['data'][i]['id'];
+                        });
+                    }
+                    break;
+                case 213:
+                    layer.msg('您太快了!慢点吧');
+                    break;
+                default:
+                    layer.msg('发生了未知的错误');
+            }
+        },
+        error: function () {
+
+        }
+    });
+}
--- js/custom.functions.js
+++ js/custom.functions.js
@@ -10,3 +10,28 @@ function getQueryString(name) {
     if (r != null) return unescape(r[2]);
     return null;
 }
+
+function getDate(val, fmt) {
+    if (!val) {
+        return '';
+    }
+    if (val && val.length === 10) {
+        val = parseInt(val * 1000);
+    }
+    let date = new Date(val);
+    let o = {
+        "M+": date.getMonth() + 1, //月份
+        "d+": date.getDate(), //日
+        "h+": date.getHours(), //小时
+        "m+": date.getMinutes(), //分
+        "s+": date.getSeconds(), //秒
+        "q+": Math.floor((date.getMonth() + 3) / 3), //季度
+        "S": date.getMilliseconds() //毫秒
+    };
+    if (/(y+)/.test(fmt))
+        fmt = fmt.replace(RegExp.$1, (date.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (let k in o)
+        if (new RegExp("(" + k + ")").test(fmt))
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
